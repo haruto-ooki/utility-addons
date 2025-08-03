6f10cca87e04c624d6a01042f79e53341063463c
haruto ooki <harutoooki173@gmail.com>
2025-08-03 12:21:08 +0900

    edited explosion.js

--- scripts/explosion.js
+++ scripts/explosion.js
@@ -4,13 +4,13 @@ const world = server.world;
 
 console.log("爆発コマンドを登録しました！");
 
-world.afterEvents.projectileHitBlock.subscribe((event)=>{
+world.afterEvents.projectileHitBlock.subscribe((event , origin)=>{
 	const projectile = event.projectile; // 投げものの情報を取得
 
 	if(projectile.typeId === "minecraft:snowball"){ // 投げものが雪玉だったら
 		const dimension = event.dimension;
 		const location = event.location;
 
-		dimension.createExplosion(location, 10);
+		origin.sourceEntity.runCommand(`fill ${location.x - 2} ${location.y - 2} ${location.z - 2} ${location.x + 2} ${location.y + 2} ${location.z + 2} tnt[explosionPower=4] destroy`);
 	}
 });
